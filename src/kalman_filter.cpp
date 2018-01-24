a83798ac466b729ca6061183bdec6291d0641f1f
Kite's Runner <shengyao@umich.edu>
2018-01-23 23:52:05 -0500

    test

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -68,19 +68,12 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   z_pred << rho, phi, rho_dot;
   VectorXd y = z - z_pred;
 
-  bool in_range = false;
-  while (in_range == false){
-   if (y(1) > 3.14159){
-     y(1) = y(1) - 6.2831;
-   }
-   else if (y(1) < -3.14159){
-     y(1) = y(1) + 6.2831;
-   }
-   else{
-     in_range = true;
-   }
+  while(y[1] > M_PI){
+    y[1] -= 2 * M_PI;
   }
-
+  while (y[1] < - M_PI) {
+    y[1] += 2 * M_PI;
+  }
   MatrixXd Ht = H_.transpose();
   MatrixXd S = H_ * P_ * Ht + R_;
   MatrixXd Si = S.inverse();
