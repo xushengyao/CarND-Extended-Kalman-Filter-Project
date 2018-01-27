23bf08364a1677f8069e52aa9458270adf6cfaab
Kite's Runner <shengyao@umich.edu>
2018-01-27 15:52:56 -0500

    debug

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -157,7 +157,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     // Laser updates
     ekf_.H_ = H_laser_;
     ekf_.R_ = R_laser_;
-  / ekf_.Update(measurement_pack.raw_measurements_);
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
