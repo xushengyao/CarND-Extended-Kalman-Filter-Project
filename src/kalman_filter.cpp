6095c91c542319f37287dafcb50653cd5bda78f7
Kite's Runner <shengyao@umich.edu>
2018-01-27 16:56:45 -0500

    test

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,98 +1,10 @@
-// #include "kalman_filter.h"
-//
-// using Eigen::MatrixXd;
-// using Eigen::VectorXd;
-//
-// // Please note that the Eigen library does not initialize
-// // VectorXd or MatrixXd objects with zeros upon creation.
-//
-// KalmanFilter::KalmanFilter() {}
-//
-// KalmanFilter::~KalmanFilter() {}
-//
-// void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-//                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-//   x_ = x_in;
-//   P_ = P_in;
-//   F_ = F_in;
-//   H_ = H_in;
-//   R_ = R_in;
-//   Q_ = Q_in;
-// }
-//
-// void KalmanFilter::Predict() {
-//   /**
-//   TODO:
-//     * predict the state
-//   */
-//   x_ = F_ * x_;
-//   P_ = F_ * P_ * F_.transpose() + Q_;
-// }
-//
-// void KalmanFilter::Update(const VectorXd &z) {
-//   /**
-//   TODO:
-//     * update the state by using Kalman Filter equations
-//   */
-//   VectorXd z_pred = H_ * x_;
-//   VectorXd y = z - z_pred;
-//   MatrixXd S = H_ * P_ * H_.transpose() + R_;
-//   MatrixXd K = P_ * H_.transpose() * S.inverse();
-//
-//   //new estimate
-//   x_ = x_ + (K * y);
-//   long x_size = x_.size();
-//   MatrixXd I = MatrixXd::Identity(x_size, x_size);
-//   P_ = (I - K * H_) * P_;
-// }
-//
-// void KalmanFilter::UpdateEKF(const VectorXd &z) {
-//   /**
-//   TODO:
-//     * update the state by using Extended Kalman Filter equations
-//   */
-//   //recover state parameters
-//   float rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
-//   //float phi = atan2(x_(1), x_(0));
-//   float phi;
-//   if (fabs(x_(0)) < 0.0001) {
-//     phi = 0;
-// 	}
-// 	else {
-// 		phi = atan2(x_(1), x_(0));
-// 	}
-//
-//   float rho_dot;
-//   if (fabs(rho) < 0.0001) {
-//     rho_dot = 0;
-//   } else {
-//     rho_dot = (x_(0)*x_(2) + x_(1)*x_(3))/rho;
-//   }
-//
-//   VectorXd z_pred = VectorXd(3);
-//   z_pred << rho, phi, rho_dot;
-//   VectorXd y = z - z_pred;
-//
-//   while(y[1] > M_PI){
-//     y[1] -= 2 * M_PI;
-//   }
-//   while (y[1] < - M_PI) {
-//     y[1] += 2 * M_PI;
-//   }
-//   MatrixXd S = H_ * P_ * H_.transpose() + R_;
-//   MatrixXd K = P_ * H_.transpose() * S.inverse();
-//
-//   //new estimate
-//   x_ = x_ + (K * y);
-//   long x_size = x_.size();
-//   MatrixXd I = MatrixXd::Identity(x_size, x_size);
-//   P_ = (I - K * H_) * P_;
-// }
-#include <math.h>       /* atan2 */
 #include "kalman_filter.h"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+
+// Please note that the Eigen library does not initialize
+// VectorXd or MatrixXd objects with zeros upon creation.
 
 KalmanFilter::KalmanFilter() {}
 
@@ -114,8 +26,7 @@ void KalmanFilter::Predict() {
     * predict the state
   */
   x_ = F_ * x_;
-  MatrixXd Ft = F_.transpose();
-  P_ = F_ * P_ * Ft + Q_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -125,11 +36,8 @@ void KalmanFilter::Update(const VectorXd &z) {
   */
   VectorXd z_pred = H_ * x_;
   VectorXd y = z - z_pred;
-  MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
-  MatrixXd PHt = P_ * Ht;
-  MatrixXd K = PHt * Si;
+  MatrixXd S = H_ * P_ * H_.transpose() + R_;
+  MatrixXd K = P_ * H_.transpose() * S.inverse();
 
   //new estimate
   x_ = x_ + (K * y);
@@ -143,22 +51,36 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  //recover state parameters
   float rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
-  float phi = atan2(x_(1), x_(0));
+  //float phi = atan2(x_(1), x_(0));
+  float phi;
+  if (fabs(x_(0)) < 0.0001) {
+    phi = 0;
+	}
+	else {
+		phi = atan2(x_(1), x_(0));
+	}
+
   float rho_dot;
   if (fabs(rho) < 0.0001) {
     rho_dot = 0;
   } else {
     rho_dot = (x_(0)*x_(2) + x_(1)*x_(3))/rho;
   }
-  VectorXd z_pred(3);
+
+  VectorXd z_pred = VectorXd(3);
   z_pred << rho, phi, rho_dot;
   VectorXd y = z - z_pred;
-  MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
-  MatrixXd PHt = P_ * Ht;
-  MatrixXd K = PHt * Si;
+
+  while(y[1] > M_PI){
+    y[1] -= 2 * M_PI;
+  }
+  while (y[1] < - M_PI) {
+    y[1] += 2 * M_PI;
+  }
+  MatrixXd S = H_ * P_ * H_.transpose() + R_;
+  MatrixXd K = P_ * H_.transpose() * S.inverse();
 
   //new estimate
   x_ = x_ + (K * y);
