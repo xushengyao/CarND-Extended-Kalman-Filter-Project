298e34c55a019b8a3d8c597b65afc6f4366a8087
Kite's Runner <shengyao@umich.edu>
2018-01-27 16:48:30 -0500

    test

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -86,5 +86,5 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   x_ = x_ + (K * y);
   long x_size = x_.size();
   MatrixXd I = MatrixXd::Identity(x_size, x_size);
-  P_ = (I - K * H_) * P_;
+//  P_ = (I - K * H_) * P_;
 }
